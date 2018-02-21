84cda5a955e6a1d6ba696d88dca2992602852a21
Ilya Kharlamov <ikharlamov@griddynamics.com>
2018-02-21 14:29:17 +0300

    add metabase monitor script for cloudwatch

---
+++ monitor.sh
@@ -0,0 +1,14 @@
+#!/bin/env bash
+InstanceID=$(curl -s "http://169.254.169.254/latest/meta-data/instance-id")
+TIMESTAMP=$(date +%Y-%m-%d"T"%H:%M:%SZ)
+METABASE_STATUS=$(curl -s 'https://mb-pilot.griddynamics.net/api/health' | grep -oP "status\"\:\"\K\w+")
+
+if [ $METABASE_STATUS == "ok" ];
+    then
+        METABASE_UP=1
+    else
+        METABASE_UP=0
+fi
+
+export AWS_DEFAULT_REGION=eu-west-1
+aws cloudwatch put-metric-data --namespace http_endpoint --dimensions InstanceID=$InstanceID --metric-name MetabaseStatus --value $METABASE_UP --timestamp $TIMESTAMP
